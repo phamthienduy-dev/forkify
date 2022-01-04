c948b6475821020770eb5b76640f922dd7ad4d12
phamthienduy-dev <duyphamthien@outlook.com>
2022-01-04 13:34:47 +0700

    API Key update

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '88b6f30a-f74c-4c53-aa17-b02a87466185';
 export const MODAL_CLOSE_SEC = 2.5;
